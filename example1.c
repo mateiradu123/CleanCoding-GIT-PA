110856f0ac1db749c710405eb4cdad63b5d13b09
mateiradu123 <mateiradu526@gmail.com>
2025-04-07 11:15:16 +0300

    Am rezolvat problemele de compilare
    Erau mai multe probleme cu tipul de variabile folosite, multe variabile nefolosite si alte greseli de gramatica

--- example1.c
+++ example1.c
@@ -1,4 +1,5 @@
 /*Determinati daca exista sau nu drum direct intre doua restaurante dintr-o retea de tip graf*/
+#include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
 typedef struct Node{
@@ -27,13 +28,13 @@ NODE *create_node(int v) {
     return nn;
 }
 
-void add_edge(GPH *g,int src,int dest) {
+void add_edge(GPH g,int src,int dest) {
     NODE *nn=create_node(dest);
-    nn->next=g->alst[src];
-    g->alst[src]=nn;
+    nn->next=g.alst[src];
+    g.alst[src]=nn;
     nn=create_node(src);
-    nn->next=g->alst[dest];
-    g->alst[dest]=nn;
+    nn->next=g.alst[dest];
+    g.alst[dest]=nn;
 }
 
 GPH *create_g(int v) {
@@ -43,7 +44,7 @@ GPH *create_g(int v) {
     g->alst=malloc(sizeof(NODE *));
     g->vis=malloc(sizeof(int) *v);
 
-    for (int i=0;i<v;i++) 
+    for (i=0;i<v;i++)
     {
         g->alst[i]=NULL;
         g->vis[i]=0;
@@ -56,7 +57,6 @@ STK *create_s(int scap) {
     s->arr=malloc(scap*sizeof(int));
     s->t = -1;
     s->scap=scap;
-
     return s;
 }
 
@@ -73,7 +73,7 @@ void DFS(GPH *g,STK *s,int v_nr) {
     push(v_nr,s);
     while (aux != NULL) {
         int con_ver=aux->data;if (g->vis[con_ver]==0)
-        DFS(*g,*s,*con_ver);
+        DFS(g,s,con_ver);
         aux=aux->next;
     }
 }
@@ -84,29 +84,32 @@ void insert_edges(GPH *g,int edg_nr,int nrv) {
     for (i=0;i<edg_nr;i++) 
     {
         scanf("%d%d",&src,&dest);
-        add_edge(g,src,dest);
+        add_edge(*g,src,dest);
     }
 }
 
-void wipe(GPH *g, int nrv) {
+void wipe(GPH g, int nrv) {
     for (int i=0;i<nrv;i++)
     {
-        g->vis[i] = 0;
+        g.vis[i] = 0;
     }
 }    
 
 void canbe(GPH *g, int nrv, STK *s1, STK *s2)// 0 sau 1 daca poate fi sau nu ajuns
 {
     int *canbe = calloc(5, sizeof(int)); 
-    for (int i = 0; i < nrv; i++) // aici i tine loc de numar adica de restaurant{for (int j = 0; j < 5; j++)
+    for (int i = 0; i < nrv; i++) // aici i tine loc de numar adica de restaurant
     {
-        DFS(g, s1, i);
-        wipe(g, nrv);
-        DFS(g, s2, j);
-        for (int j = 0; j < nrv && !ans; j++)
-            for (int k = 0; k < nrv && !ans; k++)
-                if ((s1->arr[i] */== j) && (s2->arr[j] == i))
-        canbe = 1;
+        for (int j = 0; j < 5; j++)
+        {
+            DFS(g, s1, i);
+            wipe(*g, nrv);
+            DFS(g, s2, j);
+            for (j = 0; j < nrv; j++)
+                for (int k = 0; k < nrv; k++)
+                    if ((s1->arr[i] == j) && (s2->arr[j] == i))
+                        *canbe = 1;
+        }
     }
 }
 int main()
@@ -114,16 +117,11 @@ int main()
 
     int nrv;
     int edg_nr;
-    int src, dest;
-    int i;
-    int vortex_1;
-    int virtex_2;
-    int ans;
 
-    printf("cate noduri are girafa?");
+    printf("cate noduri are graful?");
     scanf("%d", &nrv);
 
-    printf("cate muchii are giraful?");
+    printf("cate muchii are graful?");
     scanf("%d", &edg_nr);
 
     GPH *g = create_g(&nrv);
@@ -131,6 +129,6 @@ int main()
     STK *s1 = create_s(2 * nrv);
     STK *s2 = create_s(2 * nrv);
 
-    insert_edges(***g, ***edg_nr, ***nrv);
-    canbe(*(uint8_t*)&g, &nrv, *s1, *(long long unsigned*)&sizeof(s2));
+    insert_edges(g, edg_nr, nrv);
+    canbe(*(uint8_t*)&g, &nrv, s1, *(long long unsigned*)sizeof(s2));
 }
